acb109473694f7d62843db07a59c764fc02dd1b7
Sanyam <sanyamsharmap@gmail.com>
2023-12-06 23:03:46 +0530

    Updates

---
+++ precache-manifest.853dcc6b9305024f4e609f95479bf599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2576f71478f8fbb4e3bed643b4c77b6",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "561b602f9244956bb525",
+    "url": "/TextUtils/static/css/2.b24350a8.chunk.css"
+  },
+  {
+    "revision": "df8abd9cb49d05f614af",
+    "url": "/TextUtils/static/css/main.996b5c06.chunk.css"
+  },
+  {
+    "revision": "561b602f9244956bb525",
+    "url": "/TextUtils/static/js/2.ac1b076a.chunk.js"
+  },
+  {
+    "revision": "9ce0a34b6e7c072e8ae95df9bcbfbe1b",
+    "url": "/TextUtils/static/js/2.ac1b076a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f8e6b58432a3b82576",
+    "url": "/TextUtils/static/js/3.c350cee0.chunk.js"
+  },
+  {
+    "revision": "df8abd9cb49d05f614af",
+    "url": "/TextUtils/static/js/main.0fc36883.chunk.js"
+  },
+  {
+    "revision": "61e1001b561337bac674",
+    "url": "/TextUtils/static/js/runtime-main.a0af08a1.js"
+  }
+]);
